3a98d8d292ab9a267dd753e0fbde909af04ea8d4
durgesh-chaudhari-trj <durgesh.chaudhari@thereturnjourney.com>
2025-11-04 18:46:44 +0530

    abstraction.ts file updated

--- Lec-02:OOP/abstraction/abstraction.ts
+++ Lec-02:OOP/abstraction/abstraction.ts
@@ -7,11 +7,11 @@ class Circle implements Shape {
   constructor(private radius: number) {}
 
   area(): number {
-    return 3.14 * this.radius * this.radius;
+    return Math.PI * this.radius * this.radius;
   }
 
   perimeter(): number {
-    return 2 * 3.14 * this.radius;
+    return 2 * Math.PI * this.radius;
   }
 }
 
